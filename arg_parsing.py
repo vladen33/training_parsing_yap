eb8555407e34c69a9a030e4231efd45163f46655
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-02 10:59:53 +0300

    Commit 07

--- arg_parsing.py
+++ arg_parsing.py
@@ -1,10 +1,27 @@
-import sys
+import argparse
+
+MURZIK = '=^..^=...../'
 
 if __name__ == '__main__':
-    # Печать всех аргументов.
-    print('All arguments:', sys.argv)
-    # Проверка того, чтобы при запуске программы после
-    # названия скрипта было введено имя.
-    if len(sys.argv) == 2:
-        # Печать первого элемента после названия скрипта.
-        print('Hello,', sys.argv[1])
+    # Инициализация парсера аргументов с описанием.
+    parser = argparse.ArgumentParser(description='Вежливый скрипт')
+    parser.add_argument('name', help='Имя')
+    parser.add_argument('-s', '--surname', help='Фамилия')
+    parser.add_argument('-c', '--city', help='Город',
+                        choices=['Chekhov', 'Dublin', 'Minsk', 'Simbirsk'])
+    parser.add_argument('-m', '--murzik', action='store_false',
+                        help=f'Отправить {MURZIK}')
+    # Извлечение аргументов командной строки в переменную args.
+    args = parser.parse_args()
+
+    parts = []
+
+    parts.append(f'Hello, {args.name}')
+    if args.surname is not None:
+        parts.append(args.surname)
+    if args.city is not None:
+        parts.append(f'from {args.city}')
+    if args.murzik:
+        parts.append(MURZIK)
+
+    print(*parts)
